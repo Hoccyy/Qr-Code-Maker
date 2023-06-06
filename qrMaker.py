9aace386721381591e4527f0ba86c0467c86bc22
Hoccyy <94509399+Hoccyy@users.noreply.github.com>
2023-06-06 18:49:35 -0400

    Update qrMaker.py

--- qrMaker.py
+++ qrMaker.py
@@ -7,14 +7,16 @@ qr = qrcode.QRCode(version=1, box_size=10, border=4)
 # Add data to the QR code
 data = input("Enter Data: ")
 
-''' ~ remove the comment symbols from line 2 and here to avoid making blank Qrs or filter as you see fit
+'''
+Use line 2 and the following if statement to prevent blank Qr Codes/filter the content
+
 if len(data) == 0 or data == None:
     sys.exit()
 '''
 
 qr.add_data(data)
 
-# Generate the QR code image
+# Generating the QR code image
 qr.make(fit=True)
 try:
     img = qr.make_image(fill_color=input("Fill? : "), back_color=input("Background? (empty = Blank): "))
